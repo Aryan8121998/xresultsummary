fa2141a7303d4da6e80e956ff7ac805a0a828102
Aryan <rahulkathait1998@gmail.com>
2025-06-06 23:59:44 +0530

    first

--- src/App.js
+++ src/App.js
@@ -3,22 +3,30 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div className='main'>
+    <div className='result'>
+<h1>Your Result</h1>
+<div className='circle'><p className='average-marks'><span className='score'>76</span> <br/> <span className='of100'>0f 100</span></p></div>
+
+<p className='conclusion'><span className='highlight'>Great</span><br/>You scored higher than  65% of the people who have taken these tests</p>
     </div>
+    <div className='summary'>
+<h1 className='summary-heading'>Summary</h1>
+<p className='reaction'>Reaction 80/100</p>
+<p className='memory'>Memory 92/100</p>
+<p className='verbal'>Verbal 61/100</p>
+<p className='visual'>Visual 72/100</p>
+<button className='btn'>Continue</button>
+    </div>
+
+      </div>
+      <footer className='footer'>
+        Challenge by <a href='https://www.crio.do/'>Crio</a> Coded by <a href="https://github.com/Aryan8121998">Aryan</a>
+      </footer>
+    </>
+      
+    
   );
 }
 
