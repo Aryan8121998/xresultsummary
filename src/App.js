4de72f35206d853a63a07ac10a8da66ec09dfab2
Aryan <rahulkathait1998@gmail.com>
2025-06-07 00:11:02 +0530

    third

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
 <h1>Your Result</h1>
 <div className='circle'><p className='average-marks'><span className='score'>76</span> <br/> <span className='of100'>0f 100</span></p></div>
 
-<p className='conclusion'><span className='highlight'>Great</span><br/>you scored higher than  65% of the people who have taken these tests</p>
+<p className='conclusion'><span className='highlight'>Great</span><br/>you scored higher than 65% of the people who have taken these tests</p>
     </div>
     <div className='summary'>
 <h1 className='summary-heading'>Summary</h1>
